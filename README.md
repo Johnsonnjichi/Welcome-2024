6e19d0ba82ef4aa48e8191347cc40b6d3d58ab8b
Johnson <johnsonnjichi@gmail.com>
2024-01-04 01:30:14 +0300

    Git testing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Welcome-2024
+Happy 2024 Developers!!
+
+Welcome and success to all as you make 2024 better with lines of code!!
---
+++ app.py
@@ -0,0 +1,6 @@
+print("HAPPY NEW YEAR 2024>>>!!")
+print("HAPPY HACKING 2024>>>>!!")
+
+
+print("Laptop Setup!")
+print("Git and Github Testing")
